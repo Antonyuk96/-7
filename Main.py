1bde31fb5ec083a7e26747d0aaf6b10dcba47409
Antonyuk96 <113938370+Antonyuk96@users.noreply.github.com>
2023-01-21 19:51:26 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,10 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from os.path import exists
+>>> path = 'data-2016-10-10T00-00-structure-2016-10-10T00-00.csv'
+>>> path = 'data-2016-10-10T00-00-structure-2016-10-10T00-00.csv'
+>>> valid = exists(path)
+>>> #if not valid:
+...     #creating()
+... 
+... #writing_scv()
