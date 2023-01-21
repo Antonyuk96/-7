de65c73e29f1c05a1b57e7d3f77fe886f156eedb
Antonyuk96 <113938370+Antonyuk96@users.noreply.github.com>
2023-01-21 18:13:43 +0300

    Add files via upload

---
+++ User_interface.py
@@ -0,0 +1,537 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+def get_info ():
+   info = []
+    name = input('Введите орган управления дорожным хозяйством,в оперативное управление которых переданы автомобильные дороги федерального значения: ')
+    
+SyntaxError: unexpected indent
+ name = input('Введите орган управления дорожным хозяйством,в оперативное управление которых переданы автомобильные дороги федерального значения: ')
+ 
+SyntaxError: unexpected indent
+name = input('Введите орган управления дорожным хозяйством,в оперативное управление которых переданы автомобильные дороги федерального значения: ')
+Введите орган управления дорожным хозяйством,в оперативное управление которых переданы автомобильные дороги федерального значения: 
+ info.append(name)
+ 
+SyntaxError: unexpected indent
+ info.append(last_name)
+ 
+SyntaxError: unexpected indent
+ info.append(name)
+ 
+SyntaxError: unexpected indent
+info.append(last_name)
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    info.append(last_name)
+NameError: name 'info' is not defined
+info = []
+name = input('Введите орган управления дорожным хозяйством,в оперативное управление которого переданы автомобильные дороги федерального значения: ')
+Введите орган управления дорожным хозяйством,в оперативное управление которого переданы автомобильные дороги федерального значения: 
+info.append(name)
+phone_number = ''
+valid =False
+while not valid:
+        try:
+            phone_number = input('Введите номер телефона: ')
+           if len(phone_number) != 11:
+               
+SyntaxError: unindent does not match any outer indentation level
+if len(phone_number) != 11:
+    print('В номере телефона должно быть 10 цифр.')
+    else:
+        
+SyntaxError: invalid syntax
+else:
+    
+SyntaxError: invalid syntax
+else:
+    
+SyntaxError: invalid syntax
+else:
+    
+SyntaxError: invalid syntax
+print('В номере телефона должно быть 10 цифр.')
+В номере телефона должно быть 10 цифр.
+else:
+                phone_number = int(phone_number)
+                valid = True
+                
+SyntaxError: invalid syntax
+else:
+                phone_number = int(phone_number)
+                valid = True
+                
+SyntaxError: invalid syntax
+if len(phone_number) != 11:
+    print('В номере телефона должно быть 10 цифр.')
+    else:
+                phone_number = int(phone_number)
+                valid = True
+                
+SyntaxError: invalid syntax
+else:
+                phone_number = int(phone_number)
+                valid = True
+                
+SyntaxError: invalid syntax
+if len(phone_number) != 11:
+    print('В номере телефона должно быть 10 цифр.')
+    else:
+        
+SyntaxError: invalid syntax
+phone_number = ''
+valid =False
+while not valid:
+        try:
+            phone_number = input('Введите номер телефона: ')
+            while not valid:
+        try:
+            phone_number = input('Введите номер телефона: ')
+            
+SyntaxError: expected an indented block after 'while' statement on line 4
+while not valid:
+        try:
+            phone_number = input('Введите номер телефона: ')
+            if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+                else:
+                phone_number = int(phone_number)
+                valid = True
+                
+SyntaxError: invalid syntax
+else:
+                phone_number = int(phone_number)
+                valid = True
+                
+SyntaxError: invalid syntax
+SyntaxError: invalid syntax
+SyntaxError: incomplete input
+ else:
+                phone_number = int(phone_number)
+                
+SyntaxError: unexpected indent
+ else:
+                phone_number = int(phone_number)
+                
+SyntaxError: unexpected indent
+else:
+                phone_number = int(phone_number)
+                
+SyntaxError: invalid syntax
+while not valid:
+        try:
+            phone_number = input('Введите номер телефона: ')
+            f len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+            else:
+                phone_number = int(phone_number)
+                valid = True
+                
+SyntaxError: invalid syntax
+while not valid:
+        try:
+            phone_number = input('Введите номер телефона: ')
+            if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+                else:
+                phone_number = int(phone_number)
+                valid = True
+                
+SyntaxError: invalid syntax
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 11 цифр.')
+            else:
+                phone_number = int(phone_number)
+                valid = True
+                
+SyntaxError: unindent does not match any outer indentation level
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 11 цифр.')
+            else:
+                
+SyntaxError: unindent does not match any outer indentation level
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 11 цифр.')
+                if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+                
+SyntaxError: expected an indented block after 'if' statement on line 3
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+                else:
+                    
+SyntaxError: invalid syntax
+else:
+    
+SyntaxError: invalid syntax
+else:
+    
+SyntaxError: invalid syntax
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+    phone_number = int(phone_number)
+    valid = True
+   except:
+       
+SyntaxError: unindent does not match any outer indentation level
+except:
+    
+SyntaxError: invalid syntax
+except:
+            print('Номер телефона должен состоять только из цифр.')
+            
+SyntaxError: invalid syntax
+except:
+            print('Номер телефона должен состоять только из цифр.')
+            
+SyntaxError: invalid syntax
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+   except:
+            print('Номер телефона должен состоять только из цифр.')
+            
+SyntaxError: invalid syntax
+except:
+            print('Номер телефона должен состоять только из цифр.')
+            
+SyntaxError: invalid syntax
+except:
+    
+SyntaxError: invalid syntax
+ else:
+     
+SyntaxError: unexpected indent
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+  except:
+            print('Номер телефона должен состоять только из цифр.')
+            
+SyntaxError: unindent does not match any outer indentation level
+except:
+            print('Номер телефона должен состоять только из цифр.')
+            
+SyntaxError: invalid syntax
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+  except:
+      
+SyntaxError: unindent does not match any outer indentation level
+except:
+    
+SyntaxError: invalid syntax
+ except:
+     
+SyntaxError: unexpected indent
+   except:
+       
+SyntaxError: unexpected indent
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+except:
+    
+SyntaxError: invalid syntax
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+            else:
+                phone_number = int(phone_number)
+                valid = True
+        except:
+            print('Номер телефона должен состоять только из цифр.')
+            
+SyntaxError: unindent does not match any outer indentation level
+>>> if len(phone_number) != 11:
+...                 print('В номере телефона должно быть 10 цифр.')
+... else:
+...    phone_number = int(phone_number)
+...    valid = True
+SyntaxError: invalid syntax
+>>> if len(phone_number) != 11:
+...                 print('В номере телефона должно быть 10 цифр.')
+... else:
+...    phone_number = int(phone_number)
+...    valid = True
+
+SyntaxError: invalid syntax
+>> if len(phone_number) != 11:
+...                 print('В номере телефона должно быть 10 цифр.')
+... else:
+...    phone_number = int(phone_number)
+...    valid = True
+SyntaxError: invalid syntax
+if len(phone_number) != 11:
+...                 print('В номере телефона должно быть 10 цифр.')
+... else:
+...    phone_number = int(phone_number)
+...    valid = True
+SyntaxError: expected an indented block after 'if' statement on line 1
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+            else:
+                phone_number = int(phone_number)
+                valid = True
+                
+SyntaxError: unindent does not match any outer indentation level
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+   except
+   
+SyntaxError: incomplete input
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+  except:
+      
+SyntaxError: unindent does not match any outer indentation level
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+   except:
+       
+SyntaxError: invalid syntax
+print('В номере телефона должно быть 10 цифр.')
+В номере телефона должно быть 10 цифр.
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+
+SyntaxError: invalid syntax
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+except:
+                print('Номер телефона должен состоять только из цифр.')
+                
+SyntaxError: invalid syntax
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+   except:
+       
+SyntaxError: invalid syntax
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+except:
+    
+SyntaxError: invalid syntax
+ except:
+     
+SyntaxError: unexpected indent
+ except:
+     
+SyntaxError: unexpected indent
+   except:
+       
+SyntaxError: unexpected indent
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+
+   
+В номере телефона должно быть 10 цифр.
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+except:
+    
+SyntaxError: invalid syntax
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+
+   
+В номере телефона должно быть 10 цифр.
+
+
+except:
+            print('Номер телефона должен состоять только из цифр.')
+            
+SyntaxError: invalid syntax
+   except:
+                print('Номер телефона должен состоять только из цифр.')
+                
+SyntaxError: unexpected indent
+except:
+                print('Номер телефона должен состоять только из цифр.')
+                
+SyntaxError: invalid syntax
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+   info.append(phone_number)
+   return info
+SyntaxError: incomplete input
+return info
+SyntaxError: 'return' outside function
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 10 цифр.')
+else:
+   phone_number = int(phone_number)
+   valid = True
+   info.append(phone_number)
+   info = []
+   return info
+SyntaxError: incomplete input
+def get_info ():
+   info = []
+name = input('Введите орган управления дорожным хозяйством,в оперативное управление которых переданы автомобильные дороги федерального значения: ')
+Введите орган управления дорожным хозяйством,в оперативное управление которых переданы автомобильные дороги федерального значения:
+    
+SyntaxError: invalid syntax
+name = input('Введите орган управления дорожным хозяйством,в оперативное управление которых переданы автомобильные дороги федерального значения: ')
+Введите орган управления дорожным хозяйством,в оперативное управление которых переданы автомобильные дороги федерального значения:
+    
+SyntaxError: multiple statements found while compiling a single statement
+def get_info ():
+    info = []
+    name = input('Введите орган управления дорожным хозяйством,в оперативное управление которых переданы автомобильные дороги федерального значения: ')
+   info.append(name)
+   
+SyntaxError: unindent does not match any outer indentation level
+info.append(name)
+phone_number = ''
+phone_number = ''
+valid =False
+valid =False
+while not valid:
+        try:
+            phone_number = input('Введите номер телефона: ')
+           if len(phone_number) != 11:
+                print('В номере телефона должно быть 11 цифр.')
+                
+SyntaxError: unindent does not match any outer indentation level
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 11 цифр')
+else:
+                phone_number = int(phone_number)
+                valid = True
+
+                
+В номере телефона должно быть 11 цифр
+
+
+
+
+
+
+
+
+ info.append(phone_number)
+ 
+SyntaxError: unexpected indent
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 11 цифр')
+else:
+                phone_number = int(phone_number)
+                valid = True
+                 info.append(phone_number)
+                 
+SyntaxError: unexpected indent
+ info.append(phone_number)
+ 
+SyntaxError: unexpected indent
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 11 цифр')
+else:
+                phone_number = int(phone_number)
+                valid = True
+                info.append(phone_number)
+                return info
+            
+SyntaxError: incomplete input
+return info
+SyntaxError: 'return' outside function
+f len(phone_number) != 11:
+                print('В номере телефона должно быть 11 цифр')
+else:
+                phone_number = int(phone_number)
+                valid = True
+                info.append(phone_number)
+                return info
+            
+SyntaxError: invalid syntax
+if len(phone_number) != 11:
+                print('В номере телефона должно быть 11 цифр')
+else:
+                phone_number = int(phone_number)
+                valid = True
+                info.append(phone_number)
+                return info
+            
+SyntaxError: incomplete input
+>>> if len(phone_number) != 11:
+...                 print('В номере телефона должно быть 11 цифр')
+... else:
+...                 phone_number = int(phone_number)
+...                 valid = True
+...                 info.append(phone_number)
+... return info
+SyntaxError: invalid syntax
+>>> return info
+SyntaxError: 'return' outside function
+>>> if len(phone_number) != 11:
+...                 print('В номере телефона должно быть 10 цифр')
+... else:
+...                 phone_number = int(phone_number)
+...                 valid = True
+...                 info.append(phone_number)
+... return info
+SyntaxError: invalid syntax
+>>>                 return info
+...             
+SyntaxError: unexpected indent
+>>> return info
+SyntaxError: 'return' outside function
+>>> else:
+...                 phone_number = int(phone_number)
+...                 valid = True
+...                 info.append(phone_number)
+... return info
+SyntaxError: invalid syntax
+>>> if len(phone_number) != 11:
+...                 print('В номере телефона должно быть 10 цифр')
+... else:
+...                 phone_number = int(phone_number)
+...                 valid = True
+...                 info.append(phone_number)
+... 
+...                 
+В номере телефона должно быть 10 цифр
