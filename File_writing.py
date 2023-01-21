0b389c0841562cd88041d88bf938491857661857
Antonyuk96 <113938370+Antonyuk96@users.noreply.github.com>
2023-01-21 19:20:01 +0300

    Add files via upload

---
+++ File_writing.py
@@ -0,0 +1,76 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+def writing_scv ():
+    file = 'data-2016-10-10T00-00-structure-2016-10-10T00-00'.csv'
+    
+SyntaxError: incomplete input
+file ='data-2016-10-10T00-00-structure-2016-10-10T00-00.csv'
+with open (file, 'a', encoding = 'utf-8') as data:
+     data.write(f'{info[0]};{info[1]\n')}
+     
+SyntaxError: unmatched '}'
+data.write(f'{info[0]};{info[1]\n')
+SyntaxError: incomplete input
+with open (file, 'a', encoding = 'utf-8') as data:
+    data.write(f'{info[0]};{info[1}\n')]
+    
+SyntaxError: unmatched ']'
+with open (file, 'a', encoding = 'utf-8') as data:
+    data.write(f'{info[0]};{info[1]\n')
+    
+SyntaxError: incomplete input
+data.write(f'{info[0]};{info[1}}\n')
+SyntaxError: incomplete input
+data.write(f'{info[0]};{info[1}}\n'))
+SyntaxError: unmatched ')'
+ data.write(f'info[0]};{info[1]\n')}
+ 
+SyntaxError: unexpected indent
+ data.write(f'{info[0]};{info[1]}\n')
+ 
+SyntaxError: unexpected indent
+SyntaxError: unexpected indent
+SyntaxError: incomplete input
+
+data.write(f'{info[0]};{info[1]}\n')
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    data.write(f'{info[0]};{info[1]}\n')
+NameError: name 'data' is not defined
+def writing_scv ():
+    file = 'data-2016-10-10T00-00-structure-2016-10-10T00-00'.csv'
+    
+SyntaxError: incomplete input
+def writing_scv ():
+    file = 'data-2016-10-10T00-00-structure-2016-10-10T00-00.csv'
+    with open (file, 'a', encoding = 'utf-8') as data:
+       data.write(f'{info[0]};{info[1]}\n')
+... def writing_txt ():
+...     
+SyntaxError: invalid syntax
+>>> def writing_txt ():
+...    file = 'data-2016-10-10T00-00-structure-2016-10-10T00-00.txt'
+...    with open (file, 'a', encoding = 'utf-8') as data:
+...        a.write(f'Органы управления дорожным хозяйством, в оперативное управление которых переданы автомобильные дороги федерального значения': {info[0]}\n\nНомер телефона: {info[1]}\n\n')
+...                
+SyntaxError: invalid syntax
+>>> SyntaxError: invalid syntax
+...                
+SyntaxError: incomplete input
+>>> data:
+...        data.write(f'Органы управления дорожным хозяйством, в оперативное управление которых переданы автомобильные дороги федерального значения: {info[0]}\n\nНомер телефона: {info[1]}\n\n')
+...                
+SyntaxError: invalid syntax
+>>> data.write(f'Органы управления дорожным хозяйством, в оперативное управление которых переданы автомобильные дороги федерального значения: {info[0]}\n\nНомер телефона: {info[1]}\n\n')
+...                
+Traceback (most recent call last):
+  File "<pyshell#37>", line 1, in <module>
+    data.write(f'Органы управления дорожным хозяйством, в оперативное управление которых переданы автомобильные дороги федерального значения: {info[0]}\n\nНомер телефона: {info[1]}\n\n')
+NameError: name 'data' is not defined
+>>> def writing_txt ():
+...     file = 'data-2016-10-10T00-00-structure-2016-10-10T00-00.txt'
+...     with open (file, 'a', encoding = 'utf-8') as data:
+...         data.write(f'Органы управления дорожным хозяйством, в оперативное управление которых переданы автомобильные дороги федерального значения: {info[0]}\n\nНомер телефона: {info[1]}\n\n')
+... 
+...         
+>>> 
